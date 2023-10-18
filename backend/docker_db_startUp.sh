4ec8c5ab2aa50f236a24685077f7d3059173a498
John Brian <njugunajb96@gmail.com>
2023-10-18 07:47:54 +0300

    update start up

--- backend/docker_db_startUp.sh
+++
@@ -1,4 +0,0 @@
-echo "================================== starting JiliTodo Database ==================";
-docker compose -f docker-compose.db.yml up -d  || return;
-echo "================================== done starting JiliTodo Database ==================";
-
