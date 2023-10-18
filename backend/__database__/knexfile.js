c354d06f11a66da8696be258f1075ec337a8b7a6
John Brian <njugunajb96@gmail.com>
2023-10-18 07:43:57 +0300

    update host

--- backend/__database__/knexfile.js
+++ backend/__database__/knexfile.js
@@ -2,7 +2,7 @@ require("dotenv").config({ path: "../.env" });
 module.exports = {
   client: "pg",
   connection: {
-    host: "localhost",
+    host: process.env.DATABASE_SERVER,
     user: process.env.DATABASE_USER,
     port: Number(process.env.DATABASE_PORT),
     password: process.env.DATABASE_PASSWORD,
