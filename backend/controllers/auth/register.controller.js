7b6a604dc421fd2339ebf89224ed2b99ec4a6da2
John Brian Ngugi Njuguna <njugunajb96@gmail.com>
2023-10-22 22:47:08 +0300

    add validation

--- backend/controllers/auth/register.controller.js
+++ backend/controllers/auth/register.controller.js
@@ -1,7 +1,32 @@
-const register = (req, res) => {
+const {validationResult } = require("express-validator");
+
+const registerUser = (req, res) => {
   try {
-    const {firstname, lastname} = req.body;
+    /**
+     * destructure properties
+     */
+    const { firstname, lastname, email } = req.body;
+
+    /**
+     * validate properties
+     */
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+
+    return res.status(201).json({
+      success: true,
+      message: "Registered successfully"
+    });
+
   } catch (error) {
-    console.log(error);
+    return res.status(201).json({
+      success: false,
+      message: "Internal server error"
+    });
   }
 };
+
+module.exports = { registerUser }
