6a830e95f0a7175f42244d3c19a29c64e4062546
Njuguna JohnBrian <njugunajb96@gmail.com>
2023-10-29 07:54:32 +0300

    add console clear

--- backend/server.js
+++ backend/server.js
@@ -8,16 +8,15 @@ process.on("uncaughtException", (err) => {
   process.exit(1);
 });
 
-
 /**
  * instantiate server
  */
 const server = app.listen(process.env.NODE_PORT, () => {
+  console.clear();
   console.log(
-    `Server started on PORT: ${process.env.NODE_PORT} in ${process.env.NODE_ENV} mode`
+    `Server started on PORT: ${process.env.NODE_PORT} in ${process.env.NODE_ENV} mode`,
   );
 });
-
 
 //Handle Unhandled Promise Rejections
 process.on("unhandledRejection", (err) => {
