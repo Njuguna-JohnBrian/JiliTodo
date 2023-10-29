8fc1b94629fb93d2fe83d95fcc0a8d0f053bd7b2
Njuguna JohnBrian <njugunajb96@gmail.com>
2023-10-29 16:02:39 +0300

    reduce repeated logic

--- backend/routes/auth.router.js
+++ backend/routes/auth.router.js
@@ -2,6 +2,10 @@ const express = require("express");
 const { check } = require("express-validator");
 
 const { registerUser } = require("../controllers/auth/auth.controller");
+const {
+  payloadValidationErrors,
+} = require("../helpers/payloadValidationErrors.helper");
+const { authValidations } = require("../validations/auth.validations");
 
 const auth = express.Router();
 
@@ -9,25 +13,8 @@ const authBaseUrl = "/auth/user";
 
 auth.post(
   authBaseUrl + "/register",
-  [
-    check("firstname", "Firstname is required").notEmpty().trim(),
-    check("lastname", "Lastname is required").notEmpty().trim(),
-    check("email", "Email is required").notEmpty(),
-    check("email", "Email is not valid").isEmail().normalizeEmail(),
-    check("password", "Password is required").notEmpty(),
-
-    check(
-      "password",
-      "Password must contain at least 1 digit,1 special character, 1 lowercase and 1 uppercase letter and be at least 6 characters long",
-    ).matches(
-      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$/,
-      "i",
-    ),
-    check("confirmPassword", "Confirm Password is required").notEmpty(),
-    check("confirmPassword", "Confirm Password does not match password").custom(
-      (value, { req }) => value === req.body.password,
-    ),
-  ],
+  authValidations["registration"],
+  payloadValidationErrors,
   registerUser,
 );
 
