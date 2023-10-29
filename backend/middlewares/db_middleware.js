2a9d1c415aeb5c705c0bd61562554bc263a3f8f4
Njuguna JohnBrian <njugunajb96@gmail.com>
2023-10-29 12:20:37 +0300

    fix indent

--- backend/middlewares/db_middleware.js
+++ backend/middlewares/db_middleware.js
@@ -1,7 +1,6 @@
 const { db_helper } = require("../database/helpers/db_helper");
 const { db_config } = require("../database/db_config");
 
-
 const db_middleware = [
   async (req, res, next) => {
     /**
@@ -9,14 +8,12 @@ const db_middleware = [
      */
     const connection = await db_config.client.acquireConnection();
 
-   
-
     const db_context = db_helper(connection);
 
     res.on("close", () => db_config.client.releaseConnection(connection));
 
     req.db_context = db_context;
-    
+
     next();
   },
 ];
