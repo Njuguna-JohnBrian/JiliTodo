2f50ff09b5d6eb4edf22577183b87592c04095c3
Njuguna JohnBrian <njugunajb96@gmail.com>
2023-10-29 11:57:04 +0300

    correct CatchAsyncErrorsHelper method

--- backend/helpers/catchAsyncErrors.helper.js
+++ backend/helpers/catchAsyncErrors.helper.js
@@ -3,7 +3,7 @@
  * @param handler a callback function
  * @returns {function(*, *, *): Promise<Awaited<*>>} a resolved promise or an error caught by the next middleware
  */
-const CatchAsyncErrors = (handler) => (req, res, next) =>
+const CatchAsyncErrorsHelper = (handler) => (req, res, next) =>
   Promise.resolve(handler(req, res, next)).catch(next);
 
-module.exports = { CatchAsyncErrors };
+module.exports = { CatchAsyncErrors: CatchAsyncErrorsHelper };
