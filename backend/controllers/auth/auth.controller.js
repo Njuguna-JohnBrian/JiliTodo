e1c6dd56616ceadf0ab394ad9f6fc1bc8fbc499b
Njuguna JohnBrian <njugunajb96@gmail.com>
2023-10-29 11:56:26 +0300

    handle auth errors

--- backend/controllers/auth/auth.controller.js
+++ backend/controllers/auth/auth.controller.js
@@ -1,44 +1,42 @@
 const { validationResult } = require("express-validator");
-const { hashPassword, createAuthCookie } = require("../../helpers/auth");
+const { CatchAsyncErrors } = require("../../helpers/catchAsyncErrors.helper");
+const { hashPassword, createAuthCookie } = require("../../helpers/auth.helper");
+const ErrorHandler = require("../../helpers/errorHandler.helper");
 
-const registerUser = async (req, res) => {
-  try {
-    /**
-     * destructure properties
-     */
-    let { firstname, lastname, email, password } = req.body;
+const registerUser = CatchAsyncErrors(async (req, res, next) => {
+  /**
+   * destructure properties
+   */
+  let { firstname, lastname, email, password } = req.body;
 
-    /**
-     * validate properties
-     * throw errors if present
-     */
-    const errors = validationResult(req);
+  /**
+   * validate properties
+   * throw errors if present
+   */
+  const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors["errors"] });
-    }
-
-    /**
-     * hash password
-     */
-    password = await hashPassword(password);
-
-    createAuthCookie(res, 200, {
-      firstname: firstname,
-      lastname: lastname,
-      email: email,
-    });
-
-    return res.status(200).json({
-      success: true,
-      message: "Registered successfully",
-    });
-  } catch (error) {
-    return res.status(201).json({
-      success: false,
-      message: "Internal server error",
-    });
+  if (!errors.isEmpty()) {
+    return next(
+      new ErrorHandler(errors["errors"].map((error) => error.msg).join(" / ")),
+      400,
+    );
   }
-};
+
+  /**
+   * hash password
+   */
+  password = await hashPassword(password);
+
+  createAuthCookie(res, {
+    firstname: firstname,
+    lastname: lastname,
+    email: email,
+  });
+
+  return res.status(200).json({
+    success: true,
+    message: "Registered successfully",
+  });
+});
 
 module.exports = { registerUser };
