7fe6ba7c4911cbd61b29536746608afec96843a0
Njuguna John Brian <njugunajb96@gmail.com>
2023-11-03 15:34:03 +0300

    adjust health controller res

--- backend/controllers/health.controller.js
+++ backend/controllers/health.controller.js
@@ -1,5 +1,6 @@
 const { CatchAsyncErrors } = require("../helpers/catchAsyncErrors.helper");
 const ErrorHandler = require("../helpers/errorHandler.helper");
+const { responseHelper } = require("../helpers/responseHelper");
 /**
  *
  * @param {*} req request body object
@@ -10,10 +11,7 @@ const checkHealth = CatchAsyncErrors(async (req, res, next) => {
   if (req.errored)
     return next(new ErrorHandler("Jilitodo api is running into problems"), 404);
 
-  return res.status(200).json({
-    success: true,
-    message: "Jilitodo api is healthy",
-  });
+  return responseHelper(res, 200, true, "Jilitodo api is healthy");
 });
 
 module.exports = { checkHealth };
