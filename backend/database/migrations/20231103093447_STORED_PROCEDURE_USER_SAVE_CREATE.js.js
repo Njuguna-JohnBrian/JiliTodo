8d22b7be181d0b0a38022b6a4b1cd42f045808ed
Njuguna John Brian <njugunajb96@gmail.com>
2023-11-03 15:09:10 +0300

    get rid of created by

--- backend/database/migrations/20231103093447_STORED_PROCEDURE_USER_SAVE_CREATE.js.js
+++ backend/database/migrations/20231103093447_STORED_PROCEDURE_USER_SAVE_CREATE.js.js
@@ -7,15 +7,15 @@ exports.up = async function (knex) {
     .raw(`DROP PROCEDURE IF EXISTS public.usersave(varchar, varchar, varchar, varchar, integer, inout json);
 CREATE OR REPLACE PROCEDURE public.UserSave(IN IN_FirstName character varying, IN IN_LastName character varying,
                                             IN IN_Email character varying, IN IN_PasswordHash character varying,
-                                            IN IN_CreatedBy integer, INOUT jsonResult json DEFAULT NULL::json)
+                                            INOUT jsonResult json DEFAULT NULL::json)
     LANGUAGE 'plpgsql'
 AS
 $BODY$
 
 BEGIN
-    INSERT INTO public."Users"("firstname", "lastname", email, "passwordhash", "createdby",
-                               "createddtm", "updatedby", "updateddtm")
-    VALUES (IN_FirstName, IN_LastName, IN_Email, IN_PasswordHash, IN_CreatedBy, now(), IN_CreatedBy, now())
+    INSERT INTO public."Users"("firstname", "lastname", email, "passwordhash",
+                               "createddtm", "updateddtm")
+    VALUES (IN_FirstName, IN_LastName, IN_Email, IN_PasswordHash,now(), now())
     RETURNING row_to_json("Users".*)
         INTO jsonResult;
 END ;
