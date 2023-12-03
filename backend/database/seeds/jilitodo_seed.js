db686a7853d4c592ce14434a831a27cfc851e4f4
JohnBrian <njugunajb96@gmail.com>
2023-12-03 11:45:47 +0300

    catch wrong data format errors in prod mode

--- backend/database/seeds/jilitodo_seed.js
+++ backend/database/seeds/jilitodo_seed.js
@@ -3,7 +3,7 @@ const jilitodo_seed = [
     firstname: "Jili",
     lastname: "Todo",
     email: "jilitodo@gmail.com",
-    passwordhash: "5994471abb01112afcc18159f6cc74b4f511b9980",
+    passwordhash: "5994471abb01112afcc18159f6cc74b4f511b9980", //password:jilitodoTest123!
     createdby: 1,
     createddtm: "2023-11-03 19:10:25-07",
   },
@@ -12,7 +12,6 @@ const jilitodo_seed = [
 const seed = async (knex) => {
   const userCount = await knex("Users").count("id as count").first();
   if (Number(userCount.count) === 0) {
-    await knex("Users").truncate();
     await knex("Users").insert(jilitodo_seed);
   } else {
     console.info("Users table has records\nSkipping seeding");
