d205f3e1e3c313e4a425c661c66deabe9ffe788c
JohnBrian <njugunajb96@gmail.com>
2023-12-03 15:56:39 +0300

    create user validations

---
+++ backend/validations/user.validations.js
@@ -0,0 +1,16 @@
+const { check } = require("express-validator");
+const validator = require("validator");
+
+const userValidations = {
+  updateProfile: [
+    check("userId", "User not found or does not exist").custom(
+      (value, { req }) => validator.isUUID(value, 4),
+    ),
+    check("firstname", "Firstname is empty").optional().notEmpty().trim(),
+    check("lastname", "Lastname is empty").optional().notEmpty().trim(),
+    check("email", "Email is empty").optional().notEmpty(),
+    check("email", "Email is not valid").optional().isEmail().normalizeEmail(),
+  ],
+};
+
+module.exports = { userValidations };
