2f3ae8eba35569d0a6da32fb7e9a41453d27aefb
JohnBrian <njugunajb96@gmail.com>
2023-12-05 18:24:23 +0300

    check roleid for guid type

--- backend/validations/user.validations.js
+++ backend/validations/user.validations.js
@@ -1,10 +1,10 @@
 const { check } = require("express-validator");
-const validator = require("validator");
+const { isUUID } = require("validator");
 
 const userValidations = {
   updateProfile: [
     check("userId", "User not found or does not exist").custom(
-      (value, { req }) => validator.isUUID(value, 4),
+      (value, { req }) => isUUID(value, 4),
     ),
     check("firstname", "Firstname is empty").optional().notEmpty().trim(),
     check("lastname", "Lastname is empty").optional().notEmpty().trim(),
