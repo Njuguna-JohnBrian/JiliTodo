40958335116a37ab8add9ac88270a809376d6bc9
JohnBrian <njugunajb96@gmail.com>
2023-12-05 15:50:40 +0300

    add roles seed

--- backend/database/seeds/jilitodo_seed.js
+++ backend/database/seeds/jilitodo_seed.js
@@ -1,20 +1,42 @@
-const jilitodo_seed = [
-  {
-    firstname: "Jili",
-    lastname: "Todo",
-    email: "jilitodo@gmail.com",
-    passwordhash: "5994471abb01112afcc18159f6cc74b4f511b9980", //password:jilitodoTest123!
-    createdby: 1,
-    createddtm: "2023-11-03 19:10:25-07",
-  },
-];
+const jilitodo_seed = {
+  userTableSeed: [
+    {
+      firstname: "Jili",
+      lastname: "Todo",
+      email: "jilitodo@gmail.com",
+      roleid: 1,
+      passwordhash: "5994471abb01112afcc18159f6cc74b4f511b9980", //password:jilitodoTest123!
+      createdby: 1,
+      createddtm: "2023-11-03 19:10:25-07",
+    },
+  ],
+  rolesTableSeed: [
+    {
+      rolename: "user",
+      createdby: 1,
+      createddtm: "2023-11-03 19:10:25-07",
+    },
+    {
+      rolename: "admin",
+      createdby: 1,
+      createddtm: "2023-11-03 19:10:25-07",
+    },
+  ],
+};
 
 const seed = async (knex) => {
   const userCount = await knex("Users").count("id as count").first();
+  const roleCount = await knex("Roles").count("id as count").first();
   if (Number(userCount.count) === 0) {
-    await knex("Users").insert(jilitodo_seed);
+    await knex("Users").insert(jilitodo_seed["userTableSeed"]);
   } else {
     console.info("Users table has records\nSkipping seeding");
+  }
+
+  if (Number(roleCount.count) === 0) {
+    await knex("Roles").insert(jilitodo_seed["rolesTableSeed"]);
+  } else {
+    console.info("Roles table has records\nSkipping seeding");
   }
 };
 
